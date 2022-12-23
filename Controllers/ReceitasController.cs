eae0be5964ff849509b488647899868579087fd5
Weidong-Liang <liang.decantao@gmail.com>
2022-12-23 14:37:27 -0300

    paginas criadas e linkadas

--- Controllers/ReceitasController.cs
+++ Controllers/ReceitasController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using livro_receitas.ReceitasContext;
+using livro_receitas.Models;
 
 namespace livro_receitas.Controllers
 {
@@ -17,8 +18,86 @@ namespace livro_receitas.Controllers
 
         public IActionResult Index()
         {
-            return View ();
+            var receitas = _context.receitas.ToList();
+            return View (receitas);
         }
         
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar (Receitas receitas)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.receitas.Add(receitas);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(receitas);
+        }
+        public IActionResult Editar (int id)
+        {
+            var receitas = _context.receitas.Find(id);
+
+            if (receitas == null)
+            {
+                return NotFound();
+            }
+            return View (receitas);
+        }
+        
+        [HttpPost]
+        public IActionResult Editar (Receitas receitas)
+        {
+            var receitaCadastrada = _context.receitas.Find(receitas.Id);
+
+            receitaCadastrada.Nome = receitas.Nome;
+            receitaCadastrada.Descrição = receitas.Descrição;
+            receitaCadastrada.Igredientes = receitas.Igredientes;
+            receitaCadastrada.Tipo = receitas.Tipo;
+            receitaCadastrada.ModoDePreparo = receitas.ModoDePreparo;
+            receitaCadastrada.LinkYoutube = receitas.LinkYoutube;
+
+            _context.receitas.Update(receitaCadastrada);
+            _context.SaveChanges();
+
+            return RedirectToAction (nameof(Index));
+        }
+
+        public IActionResult Ver (int id)
+        {
+            var receitas = _context.receitas.Find(id);
+
+            if(receitas == null)
+            {
+                return NotFound();
+            }
+            return View(receitas);
+        }
+
+        public IActionResult Deletar (int id)
+        {
+            var receitas = _context.receitas.Find(id);
+
+            if(receitas == null)
+            {
+                return NotFound();
+            }
+            return View(receitas);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar (Receitas receitas)
+        {
+            var receitaCadastrada = _context.receitas.Find(receitas.Id);
+
+            _context.receitas.Remove(receitaCadastrada);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
